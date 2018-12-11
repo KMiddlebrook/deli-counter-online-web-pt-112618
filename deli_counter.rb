4f1f153c75ba899b05283fd14ae9e615f6912e92
Kaitlyn Middlebrook <kmiddlebrook2000@gmail.com>
2018-12-11 04:54:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+require "pry"
+katz_deli = []
+
+def line(array)
+  if array.size == 0
+    puts "The line is currently empty."
+  else
+   line_people = array.collect.with_index(1) do |name, index|
+      "#{index}. #{name}"
+    end
+    puts "The line is currently: " + line_people.join(" ")
+  end
+end
+
+def take_a_number(array)
+  
+end
